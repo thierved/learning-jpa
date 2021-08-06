f09bee49449ec6ffae68af33a33ff4fa33a648ec
thierno sadou diallo <thierved01@gmail.com>
2021-08-06 15:42:51 +0000

    Initial commit

---
+++ src/main/java/com/sadumatic/jpa/learningjpa/LearningJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sadumatic.jpa.learningjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadumatic/jpa/learningjpa/LearningJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadumatic.jpa.learningjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
